4c87d860789b2019be6a2de99f9732c7b6467ac5
Anne Chinn <achinn@nwlink.com>
2021-06-21 19:24:10 -0700

    got db connection working with config file

--- full-stack/back-end/server.js
+++ full-stack/back-end/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const connectDB = require('./db.js');
+const { port } = require('./config');
+
 const app = express();
+
+connectDB();
 
 app.put('/api', (req, res)=> {
   res.send('Hello World!');
 });
 
-const port = 3000;
 app.listen(port, ()=> {
   console.log(`Server started on port ${port}`);
 });
