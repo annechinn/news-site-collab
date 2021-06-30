92e2eaceaa9ac8432b21f22eff0199e04e6fe04e
Anne Chinn <achinn@nwlink.com>
2021-06-30 01:25:50 -0700

    working on routing

--- front-end/src/components/ArticleCard/ArticleCard.js
+++ front-end/src/components/ArticleCard/ArticleCard.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Link} from "react-router-dom"; 
+import TopicPill from '../TopicPill/TopicPill';
 import './ArticleCard.css';
 
 function ArticleCard({article}) {
   const articleLink = `/section/${article.topic._id}/${article._id}`;
-  const classString =  `category category-${article.topic.name}`;
   return (
     <article key={article.id}>
     <img src={article.imageURL} alt="" />
-    <span className={classString}>{article.topic.name}</span>
+    <TopicPill name={article.topic.name} title={article.topic.title}/>
     <h3><Link to={articleLink}>{article.title}</Link></h3>
     <p>{article.abstract}</p>
     
--- front-end/src/components/ShowcaseArticle/ShowcaseArticle.js
+++ front-end/src/components/ShowcaseArticle/ShowcaseArticle.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './ShowcaseArticle.css';
 
-import CategoryPill from '../CategoryPill/CategoryPill';
+import TopicPill from '../TopicPill/TopicPill';
 
 function ShowcaseArticle({article}) {
   return (
     <section style={{ background: `url(${article.jumboImageURL||article.imageURL}) center/cover` }} className="showcase">
-      <CategoryPill name={article.topic.name} title={article.topic.title}/>
+      <TopicPill name={article.topic.name} title={article.topic.title}/>
       <h1>{article.title}</h1>
       <p>{article.abstract}</p>
       <a href="" className="btn">Learn More</a>
