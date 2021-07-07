1698234c095b59c587a033730e01f00d50189cf4
jacob3657 <82230819+jacob3657@users.noreply.github.com>
2021-07-07 10:59:20 -0700

    n

---
+++ front-end/src/containers/ArticleSection/ArticleSection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ArticleSection.css';
+import ShowcaseArticle from '../../components/ShowcaseArticle/ShowcaseArticle';
+import ArticleGrid from '../../containers/ArticleGrid/ArticleGrid'; 
+function ArticleSection(){
+    return(
+        <>
+        <ShowcaseArticle
+        imageURL="https://images.pexels.com/photos/130621/pexels-photo-130621.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+        title="Lorem ipsum dolor sit amet."
+        abstract="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et ea
+        impedit libero, beatae animi provident nesciunt molestias ipsam nemoad."
+        topic="science"/>
+    
+        <ArticleGrid/>
+        </>
+
+    )
+} 
+export default ArticleSection;
